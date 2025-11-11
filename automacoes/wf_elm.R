71aaecf1226f937b512938ff29889a17a4df04bb
Carlos Henrique <carloshen1307@gmail.com>
2025-11-11 04:54:25 -0300

    update elm

--- automacoes/wf_elm.R
+++ automacoes/wf_elm.R
@@ -24,7 +24,7 @@ for (ds in datasets) {
       name <- get_names(filename, cases[[i]])
       print(name)
       tryCatch({
-        novo_resultado <- run_ml(df[[ts]], name, ts_elm(), test_size=test_size, params=cases[[i]], stgy=list(sa=TRUE, ro=FALSE))
+        novo_resultado <- run_ml(df[[ts]], name, ts_elm(), test_size=test_size, params=cases[[i]], stgy=list(sa=TRUE, ro=FALSE, image=FALSE))
         results2 <- rbind(results2, novo_resultado)
       }, error = function(e) {
         print('erro')
@@ -35,6 +35,49 @@ for (ds in datasets) {
         writeLines(as.character(e), error_file)
       })
     }
+    
+    
+    # Ordenar results2 por R² decrescente e pegar os 3 melhores
+    if (nrow(results2) > 0) {
+      # Ordenar por R² decrescente
+      results2_ordenado <- results2[order(-results2$r2), ]
+      
+      melhores_3 <- head(results2_ordenado, 3)
+      
+      # Rodar run_ml_with_image para os 3 melhores
+      cases_melhor_3 <- list()
+      
+      for (j in 1:nrow(melhores_3)) {
+        
+        cases_melhor_3 <- get_params_from_name(melhores_3$instance[j])
+        
+        sw_size2 <- c(cases_melhor_3$params$sw_size)
+        
+        preprocess2 <- switch(
+          cases_melhor_3$params$ranges$norm,
+          "an" = list(ts_norm_an()),
+          "ean" = list(ts_norm_ean()),
+          "gminmax" = list(ts_norm_gminmax()),
+          "swminmax" = list(ts_norm_swminmax()),
+          "diff" = list(ts_norm_diff()),
+          "default" = list(ts_norm_none())
+        )
+        
+        augment2 <- list(ts_aug_none())
+        
+        ranges2 <- list(nhid = as.numeric(cases_melhor_3$params$ranges$nhid),
+                        actfun = cases_melhor_3$params$ranges$actfun)
+        
+        params2 <- list(sw_size=sw_size2, preprocess=preprocess2, augment=augment2, ranges=ranges2)
+        
+        name2 <- get_names(filename, params2)
+        
+        run_ml(df[[ts]], name2, ts_elm(), test_size=test_size, params=params2, stgy=list(sa=TRUE, ro=FALSE, image=TRUE))
+      }
+      
+      
+    }
+    
     save(results2, file=sprintf('%s.rdata', sub('/', '/results/', filename)))
   }
 }
